9ca54342d385f93c38c51946538939c8590645a6
Cosmin Atanasiu <innorogue@gmail.com>
2016-02-12 15:44:51 -0800

    Started work on buttons

--- app/client/lib/react_components/CodeBox.jsx
+++ app/client/lib/react_components/CodeBox.jsx
@@ -1,11 +1,14 @@
 CodeBox = React.createClass({
   propTypes: {
-    snippet: React.PropTypes.object
+    snippet: React.PropTypes.object,
+    field: React.PropTypes.string
   },
 
   handleChange(event) {
-    // console.log(event)
-    // CodeSnippets.update({this.props.snippet, { $set: { original: this.props.snippet.value } } });
+    updateQuery = {};
+    updateQuery[this.props.field] = this.refs.snippet.value;
+
+    CodeSnippets.update({_id: this.props.snippet._id}, {$set: updateQuery} );
     return true;
   },
 
@@ -15,15 +18,46 @@ CodeBox = React.createClass({
     }
   },
 
-  getInitialState() {
-    return {
-      result: this.props.snippet[this.props.field]
+  arrowClass() {
+    if(this.props.field == "original") {
+      return "fa fa-2x fa-arrow-circle-o-right";
+    }
+    else {
+      return "fa fa-2x fa-arrow-circle-o-left"; 
+    }
+  },
+
+  buttonContainerClass() {
+    if(this.props.field == "original") {
+      return "original-box-button-container";
+    }
+    else {
+      return ""
+    }
+  },
+
+  buttonAction() {
+    if(this.props.field == "original") {
+      
+    }
+    else {
+      
     }
   },
 
   render() {
     return (
-      <textarea className="codebox" rows="10" cols="20" value={this.state.result} onChange={this.handleChange} />
+      <div>
+        <textarea ref="snippet" 
+        defaultValue={this.props.snippet[this.props.field]}
+        className="codebox" rows="10" cols="20"
+        onChange={this.handleChange} />
+        <div className={this.buttonContainerClass}>
+          <a onClick={this.buttonAction} className="button">
+            <i className={this.arrowClass}></i>
+          </a>
+        </div>
+      </div>
     )
   }
 });
